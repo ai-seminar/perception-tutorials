30226b5aa1c4c4fdc9fa072d09b2f71f137919cc
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2013-04-26 13:13:38 +0200

    added some more content

--- tutorial_pkg/src/tutorial.cpp
+++ tutorial_pkg/src/tutorial.cpp
@@ -19,7 +19,8 @@ int main(int argc, char **argv)
 	//path to pcd file needs to be given as a parameter
 	if(argc<2)
 	{
-		std::cerr<<"Please specify a pcd file to use"<<std::endl;
+	        std::cerr<<"Please specify a pcd file to use"<<std::endl;
+	        std::cerr<<"Run program like this: tutorial [filename.pcd]"<<std::endl;
 		exit(0);
 	}
 	//objects for reading/writing pcd files
@@ -33,16 +34,16 @@ int main(int argc, char **argv)
 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_downsampled (new pcl::PointCloud<pcl::PointXYZRGB>());
 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_plane (new pcl::PointCloud<pcl::PointXYZRGB>());
 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_clusters (new pcl::PointCloud<pcl::PointXYZRGB>());
-	// Output datasets
 	pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
 
 	//read in point cloud from pcd file
 	reader.read(argv[1],*cloud_in);
 	std::cerr<<"Input cloud has " <<cloud_in->points.size()<<" point! "<<std::endl;
 
-	//	pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
-	//	viewer.showCloud(cloud_in);
-	//	while(!viewer.wasStopped ()){}
+	//create visualization opbject
+	pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
+	viewer.showCloud(cloud_in);
+	while(!viewer.wasStopped ()){}
 
 	//removing nans from point clouds
 	std::vector<int> nans;
@@ -50,7 +51,8 @@ int main(int argc, char **argv)
 	std::cerr<<"Size of point cloud after removal of nans "<<cloud_nanles->points.size()<<"!"<<std::endl;
 	std::cerr<<"Writing output cloud nanless.pcd"<<std::endl;
 	writer.write("nanles.pcd",*cloud_nanles);
-
+	
+	//estimate normals
 	pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> ne;
 	pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGB> ());
 	ne.setInputCloud(cloud_in);
@@ -58,6 +60,7 @@ int main(int argc, char **argv)
 	ne.setRadiusSearch (0.03);
 	ne.compute(*cloud_normals);
 
+	//create cloud for visualization
 	pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloud_normal_color (new pcl::PointCloud<pcl::PointXYZRGBNormal>);
 	pcl::concatenateFields(*cloud_in, *cloud_normals, *cloud_normal_color);
 	std::cerr<<"Writing output cloud normals.pcd"<<std::endl;
@@ -74,7 +77,7 @@ int main(int argc, char **argv)
 	std::cerr<<"Writing output cloud filtered.pcd"<<std::endl;
 	writer.write("filtered.pcd",*cloud_filtered);
 
-	//vocelizing cloud
+	//voxelizing cloud
 	pcl::VoxelGrid <pcl::PointXYZRGB> vg;
 	vg.setInputCloud(cloud_filtered);
 	vg.setLeafSize(0.01f,0.01f,0.01f);
@@ -82,10 +85,6 @@ int main(int argc, char **argv)
 	std::cerr<<"Point Cloud has "<<cloud_downsampled->points.size()<<" after downsampeling"<<std::endl;
 	std::cerr<<"Writing output cloud filtered.pcd"<<std::endl;
 	writer.write("downsampled.pcd",*cloud_downsampled);
-	//	pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
-	//	viewer.showCloud(cloud_downsampled);
-	//	while(!viewer.wasStopped ()){}
-
 
 	//fitting a plane to the filtered cloud
 	pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
@@ -119,7 +118,6 @@ int main(int argc, char **argv)
 
 	std::cerr<<"Writing output cloud clusters.pcd"<<std::endl;
 	writer.write("clusters.pcd",*cloud_clusters);
-
 
 	return 0;
 }
